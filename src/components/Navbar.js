843c8b0593a64088bf02f7e9182399eb9666fadf
Sanjyoti <strsanjyotidas@gmail.com>
2023-09-02 10:48:48 +0530

    final commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,14 +10,14 @@ function Navbar(props){
         <>
             <nav className={`navbar navbar-expand-lg navbar-${props.mode==="white"?"light":"dark"} bg-${props.mode==="white"?"light":"dark"}`}>
   <div className="container-fluid">
-    <a className="navbar-brand" href="/">TEXTUTILS</a>
+    <a className="navbar-brand" href="#">TEXTUTILS</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="/">Home</a>
+          <a className="nav-link active" aria-current="page" href="#">Home</a>
         </li>
 
       </ul>
